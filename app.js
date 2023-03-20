d9862a7f1ac61ded658f90ff04e3665ca6a418e8
In Kim <in.kim0128@gmail.com>
2023-03-20 03:42:13 -0400

    worked on admin loadout change

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ let countCheck = 0;
 
 const formSubmit = document.querySelector("#formSubmit");
 const reviewSubmit = document.querySelector("#reviewSubmit");
+const changeSubmit = document.querySelector("#changeSubmit");
 
 const checkAdmin = () => {
   const givenUsername = document.querySelector("#username").value;
@@ -20,6 +21,7 @@ const checkAdmin = () => {
       if (admins[curItem] === givenPassword) {
         document.querySelector(".login-success").style.display = "block";
         countCheck = 1;
+        alert("Login Success. Welcome Admin!");
       }
     }
   });
@@ -43,17 +45,47 @@ const reviewSubmitted = () => {
 
   curDiv.classList.add("reviews");
   document.querySelector(".main").appendChild(curDiv);
-  
+
   const allReviews = document.querySelectorAll(".reviews");
-  const rand = document.querySelector(".footer");
   const lastReview = allReviews[allReviews.length - 1];
   lastReview.appendChild(curUser);
-  lastReview.appendChild(curReview);  
-}
+  lastReview.appendChild(curReview);
+};
+
+const changeOrder = () => {
+  console.log("hi");
+  order = document
+    .getElementById("image-order")
+    .value;
+
+  localStorage.setItem("pushToEnd", order);
+
+  window.location.href = "./index.html";
+};
 
 if (formSubmit != null) {
   formSubmit.addEventListener("click", checkAdmin);
 }
+
 if (reviewSubmit != null) {
   reviewSubmit.addEventListener("click", reviewSubmitted);
 }
+
+if (changeSubmit != null) {
+  changeSubmit.addEventListener("click", changeOrder);
+}
+
+window.addEventListener("load", () => {
+  if (
+    window.location.pathname ===
+    "/C:/Users/inkim/Documents/UTSC%202022/CSCB20/A2/src/html/index.html"
+  ) {
+    let container = document.querySelector(".weeklyPicks-inside4");
+    console.log(localStorage.getItem("pushToEnd"));
+    let saved = container.children[localStorage.getItem("pushToEnd")]
+    console.log(saved);
+    container.removeChild(saved);
+    container.appendChild(saved);
+    localStorage.removeItem("pushToEnd");
+  }
+});
