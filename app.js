1a933030dd3968e0be48fc76155971078828574f
In Kim <in.kim0128@gmail.com>
2023-03-19 20:55:18 -0400

    modified login logic

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
+const admins = {
+  "In Kim": "123",
+  "Leon Lee": "456",
+  "Michelle Guo": "789",
+};
 
-const loginButton = document.querySelector("#login-click");
-loginButton.addEventListener("click", logInUser);
+const formSubmit = document.querySelector("#formSubmit");
 
-function logInUser(){
-    alert("No, you stinky poo");
-}
+const checkAdmin = (event) => {
+  event.preventDefault();
+
+  const givenUsername = document.querySelector("#username");
+  const givenPassword = document.querySelector("#password");
+
+  Object.keys().forEach((curItem) => {
+    if (curItem === givenUsername) {
+      if (admins[curItem] === givenPassword) {
+        document.querySelector(".login-failed").style.display = "none";
+        document.querySelector(".login-failed").style.display = "block";
+        return;
+      }
+    }
+  });
+  document.querySelector(".login-failed").style.display = "block";
+  document.querySelector(".login-failed").style.display = "none";
+};
+
+formSubmit.addEventListener("click", checkAdmin);
